27de90b8d148d5dc90d72228e5407d42497035fd
Eadgnus <de_crystal@naver.com>
2023-05-03 11:38:25 +0900

    서버 validator 추가(fetch)

--- Server/router/tweets.js
+++ Server/router/tweets.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import * as tweetController from '../controller/tweet.js';
+import { body } from 'express-validator'
+import {validate} from '../middleware/validator.js'
+
 
 
 const router = express.Router();
 
+const validateTweet = [
+    body('text')
+        .trim()
+        .isLength({min:4})
+        .withMessage('text는 최소 4자 이상 입력하세요!'),
+    validate
+]
 //GET
 // / tweets?username=:username
 router.get('/', tweetController.getTweets);
@@ -15,13 +25,16 @@ router.get('/', tweetController.getTweets);
 // /tweets/id=:id
 router.get('/:id', tweetController.getTweetsById);
 
+
+// text가 4글자 이하인 경우 에러처리 post and put 에대해서 
 // POST
 // id: Date.now().toString()
-router.post('/', tweetController.postTweets);
+router.post('/', validateTweet, tweetController.postTweets);
+
 
 // PUT
 // text만 수정
-router.put('/:id', tweetController.putTweets);
+router.put('/:id', validateTweet, tweetController.putTweets);
 
 // DELETE
 router.delete('/:id', tweetController.deleteTweets);
