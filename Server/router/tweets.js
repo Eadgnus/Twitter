c1938902e9d45ab6b1b73118df13ed98e2f304e2
Eadgnus <de_crystal@naver.com>
2023-05-04 11:36:56 +0900

    서버에 토큰 적용

--- Server/router/tweets.js
+++ Server/router/tweets.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import * as tweetController from '../controller/tweet.js';
 import { body } from 'express-validator'
-import {validate} from '../middleware/validator.js'
+import { validate } from '../middleware/validator.js'
+import { isAuth } from '../middleware/auth.js';
 
 
 
@@ -10,34 +11,34 @@ const router = express.Router();
 const validateTweet = [
     body('text')
         .trim()
-        .isLength({min:4})
+        .isLength({ min: 4 })
         .withMessage('text는 최소 4자 이상 입력하세요!'),
     validate
 ]
 //GET
 // / tweets?username=:username
-router.get('/', tweetController.getTweets);
+router.get('/', isAuth, tweetController.getTweets);
 
 
 //이따 나머지 부분 만들어봄
 
 // GET
 // /tweets/id=:id
-router.get('/:id', tweetController.getTweetsById);
+router.get('/:id', isAuth, tweetController.getTweetsById);
 
 
 // text가 4글자 이하인 경우 에러처리 post and put 에대해서 
 // POST
 // id: Date.now().toString()
-router.post('/', validateTweet, tweetController.postTweets);
+router.post('/', isAuth, validateTweet, tweetController.postTweets);
 
 
 // PUT
 // text만 수정
-router.put('/:id', validateTweet, tweetController.putTweets);
+router.put('/:id', isAuth, validateTweet, tweetController.putTweets);
 
 // DELETE
-router.delete('/:id', tweetController.deleteTweets);
+router.delete('/:id', isAuth, tweetController.deleteTweets);
 
 
 export default router;
