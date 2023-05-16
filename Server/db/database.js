1682210f00115385bcabb67d6706ec2d6aa70803
Eadgnus <de_crystal@naver.com>
2023-05-16 14:51:31 +0900

    mongoose

--- Server/db/database.js
+++ Server/db/database.js
@@ -1,21 +1,26 @@
 import { config } from '../config.js';
-import MongoDb from 'mongodb';
+import Mongoose from 'mongoose';
 
 let db;
-export async function connectDB(){
-    return MongoDb.MongoClient.connect(config.db.host)
-        .then((client) => {
-            db = client.db()
-        });
+export async function connectDB() {
+    return Mongoose.connect(config.db.host)
 }
 
 // 몽고디비는 스키마가 없음
 // 비정형 형태, 규칙 따로 x
 
-export function getUsers(){
+export function getUsers() {
     return db.collection('users');
 }
 
-export function getTweets(){
+export function getTweets() {
     return db.collection('Tweets');
+}
+
+export function useVirtualId(schema) {
+    schema.virtual('id').get(function () {
+        return this._id.toString();
+    })
+    schema.set('toJSON', { vrituals: true });
+    schema.set('toObject', { vrituals: true });
 }
